98f0b42e082ce4c311ff627cf7c9d4168bee9543
twirecki <twirecki@genesilico.pl>
2020-06-07 01:33:25 +0200

    varna for all pockets done

--- fragments_varna.py
+++ fragments_varna.py
@@ -23,75 +23,6 @@ def argument_parser():
 
 
 
-def draw_VaRNA(seq, fold_ss, result_varna_method, colormap, col):
-
-    col2 = col.replace("0.00", "0.001")
-
-    cmd = 'java -cp ' + config_paths.VaRNA_path + ' fr.orsay.lri.varna.applications.VARNAcmd -sequenceDBN ' + seq + \
-        " -structureDBN '" + fold_ss + "' -o " + result_varna_method + "_radiate.png -title '" + result_varna_method + "'  \
-          -colorMapMin '0.0' -colorMapMax '1.0' -colorMapStyle '"+colormap+"' -colorMap '" + col2 + \
-          "' -resolution 10.0 -titleSize 8"
-    if '(' not in fold_ss[1:-1]: # safety fuse for structures without any structure (only dots)
-        cmd += " -bpStyle none -bp '#ffffff'"
-    
-    os.system(cmd)
-    
-    cmd = cmd.replace("_radiate.png", "_circular.png")
-    cmd += " -algorithm circular"
-
-    os.system(cmd)
-
-    cmd = cmd.replace("circular", "line")
-    cmd = cmd.replace("_circular.png", "_line.png")
-    os.system(cmd)
-
-
-
-def draw_VaRNA_nt(seq, fold_ss, result_varna_method, colormap, col):
-
-    col2 = col.replace("0.00", "0.001")
-    
-    if greynt == 'on' and ((method == 'dms') or (method=='dms_custom') or (method =='cmct') or (method =='cmct_custom')):
-        if (method == 'dms') or (method == 'dms_custom'):
-            nt1="U"
-            nt2="G"
-        elif (method == 'cmct') or (method == 'cmct_custom'):
-            nt1="A"
-            nt2="C"
-        c=0
-        greyout=''
-        c2 = col2.split(";")
-        z = zip(seq,c2)
-        for i,j in z:
-            c +=1
-            if (i ==nt1 or i == nt2) and j!='-1.0':
-                greyout +=str(c)+','
-                c2[c-1]='0.0'
-        col2 = ';'.join(c2)
-    
-    
-    cmd = 'java -cp ' + config_paths.VaRNA_path + ' fr.orsay.lri.varna.applications.VARNAcmd -sequenceDBN ' + seq + \
-        " -structureDBN '" + fold_ss + "' -o " + result_varna_method + "_radiate.png  \
-          -colorMapMin '0.0' -colorMapMax '1.0' -colorMapStyle '"+colormap+"' -colorMap '" + col2 + \
-          "' -resolution 10.0"
-    if greynt == 'on' and ((method == 'dms') or (method=='dms_custom') or (method =='cmct') or (method =='cmct_custom')):
-        cmd +=" -basesStyle1 fill=#bdbdbd,outline=#bdbdbd,label=#bdbdbd -applyBasesStyle1on '" + greyout +"'"
-    if '(' not in fold_ss[1:-1]: # safety fuse for structures without any structure (only dots)
-        cmd += " -bpStyle none -bp '#ffffff'"
-
-    os.system(cmd)
-    
-
-    cmd = cmd.replace("_radiate.png", "_circular.png")
-    cmd += " -algorithm circular"
-    os.system(cmd)
-
-    cmd = cmd.replace("circular", "line")
-    cmd = cmd.replace("_circular.png", "_line.png")
-    os.system(cmd)
-
-
-
 def read_file(input_file):
 
     my_dict = {}
@@ -101,12 +32,13 @@ def read_file(input_file):
                 pocket_list = []
                 fragment = line.strip()
                 name = next(file).strip()
+                seq = next(file).strip()
                 ss = next(file).strip()
             else:
                 pocket = line.strip()
                 pocket_list.append(pocket) 
             
-            my_dict[fragment] = [name, ss, pocket_list]
+            my_dict[fragment] = [name, seq, ss, pocket_list]
     
     #print(my_dict)
     return my_dict
@@ -147,7 +79,7 @@ def merge_pockets(pockets_num):
     print("".join(pocket_merged))
     return("".join(pocket_merged))
             
-def get_varna_merged(name, ss, merged_pockets):
+def get_varna_merged(name, seq, ss, pockets_num):
     print("varna merged")
 
     '''
@@ -156,25 +88,76 @@ def get_varna_merged(name, ss, merged_pockets):
           -colorMapMin '0.0' -colorMapMax '1.0' -colorMapStyle '"+colormap+"' -colorMap '" + col2 + \
           "' -resolution 10.0"
     '''
+    seq_color = [[],[],[],[],[],[]]
+    #print(pockets_num)
+    for i in range(0, len(pockets_num)):
+        
+        pocket = list(pockets_num[i])
+        #print(pocket)
+        number = ""
+        for k in range(0, len(pocket)):
+            if pocket[k] != "_":
+                number += str(k+1)+","
+        #print(number)
+        seq_color[i]= number.rstrip(",")
+    #print(seq_color)
     
+    string_for_color = ''
+    
+    col1 = " -basesStyle1 fill=#FF0000,label=#FFFFFF -applyBasesStyle1on " + str(seq_color[0])
+    col2 = " -basesStyle2 fill=#228B22,label=#FFFFFF -applyBasesStyle2on " + str(seq_color[1])
+    col3 = " -basesStyle3 fill=#0000FF,label=#FFFFFF -applyBasesStyle3on " + str(seq_color[2])
+    col4 = " -basesStyle4 fill=#FF9900,label=#FFFFFF -applyBasesStyle4on " + str(seq_color[3])
+    col5 = " -basesStyle5 fill=#9900FF,label=#FFFFFF -applyBasesStyle5on " + str(seq_color[4])
+    col6 = " -basesStyle6 fill=#FFFF00,label=#FFFFFF -applyBasesStyle6on " + str(seq_color[5])
+    
+    print(len(pockets_num))
+    if  '1' not in pockets_num[0]:
+        string_for_color = ''
+    elif (len(pockets_num) == 1) and '1' in pockets_num[0]:
+        string_for_color = col1
+    elif len(pockets_num) == 2:
+        string_for_color = col1 +col2
+    elif len(pockets_num) == 3:
+        string_for_color = col1 +col2 +col3
+    elif len(pockets_num) == 4:
+        string_for_color = col1 +col2 +col3 +col4
+    elif len(pockets_num) == 5:
+        string_for_color = col1 +col2 +col3 +col4 +col5
+    elif len(pockets_num) == 6:
+        string_for_color = col1 +col2 +col3 +col4 +col5 +col6
+    else:
+        string_for_color = ''
+    #print(string_for_color)
+    print(pockets_num[0], 'pocket_num [0]')
+    #if any(c not in '123456' for c in pockets_num[0]):  # Don't use str as a name.
+     
+     #   cmd = 'java -cp ' + VaRNA_path + ' fr.orsay.lri.varna.applications.VARNAcmd ' + \
+      #  " -structureDBN '" + ss + "' -o " + name + "_linear.png -resolution 10.0"
+#    os.system(cmd)
+        #print(len(pockets_num), 'len pock num')
+    
+    cmd = 'java -cp ' + VaRNA_path + ' fr.orsay.lri.varna.applications.VARNAcmd ' + " -sequenceDBN '" + seq + "'"+\
+        " -structureDBN '" + ss + "' -o " + name + "_linear.png -resolution 10.0" + string_for_color + " -title '" + name + "' -titleSize 6"# -annotations 'P1:anchor=60'"  
 
-    cmd = 'java -cp ' + VaRNA_path + ' fr.orsay.lri.varna.applications.VARNAcmd ' + \
-        " -structureDBN '" + ss + "' -o " + name + ".png -resolution 10.0" + \
-        " -basesStyle1 fill=#FF0000 -applyBasesStyle1on 20"
     os.system(cmd)
-
+#    quit()
+    cmd +=  " -algorithm naview"
+    cmd = cmd.replace("_linear.png", "_naview.png") 
+    os.system(cmd)
+    
 def iterate_fragments(fragments):
     
     print("iterate_fragments")
 
     for pkey, item_list in fragments.items():
-        pockets_P = item_list[2]
+        pockets_P = item_list[3]
         pockets_num = P_to_numbers(pockets_P)
         fragments[pkey].append(pockets_num)
     print(fragments)
     
     for pkey, item_list in fragments.items():
-        pockets_num = item_list[3]
+        pockets_num = item_list[4]
         if len(pockets_num) > 1:
             merged_pockets = merge_pockets(pockets_num)
         else:
@@ -184,9 +167,10 @@ def iterate_fragments(fragments):
     
     for pkey, item_list in fragments.items():
         name = item_list[0]
-        ss = item_list[1]
-        merged_pockets = item_list[4]
-        get_varna_merged(name, ss, merged_pockets)
+        seq = item_list[1]
+        ss = item_list[2]
+        pockets = item_list[4]
+        get_varna_merged(name,seq, ss, pockets)
 
 def main_function():
 
